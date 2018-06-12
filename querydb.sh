95e9c6fff1b771152fced3f602eba300fd985dc5
sudo-rm-fr <39778095+sudo-rm-fr@users.noreply.github.com>
2018-06-12 21:47:59 +0100

    Update querydb.sh

--- querydb.sh
+++ querydb.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #Query database with clawback.sql
 
-DB_NAME="bitnami_espocrm"
-DB_USER="bn_espocrm"
-DB_PASS="5f454f5f33"
+DB_NAME=
+DB_USER=
+DB_PASS=
 DATE=`date +%F.%H%M`
 
 #export database every hour
